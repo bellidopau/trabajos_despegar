1692608fb8f7ea0a92b6a87950a199e58def497d
bellidopau <bellidopaulajulieta@gmail.com>
2022-10-19 00:31:31 -0300

    revision y entrega3

--- Disenios_para_MacoWins.py
+++ Disenios_para_MacoWins.py
@@ -1,10 +1,7 @@
-"""Reconocida cadena de ropa formal, con tiendas en muchas ciudades de Argentina. 
-Le ha pedido a 2Diseños, consultora de software, desarrollar un nuevo sistema para la gestión de sus ventas y stock."""
+#Reconocida cadena de ropa formal, con tiendas en muchas ciudades de Argentina le ha pedido a 2Diseños, consultora de software, desarrollar un nuevo sistema para la gestión de sus ventas y stock
 
 
-from datetime import date
-fecha = date.strftime(date.today(),"%Y-%m-$d")
-
+import time
 
 camisa = {
   "codigo": 100,
@@ -67,8 +64,21 @@ medias = {
   "precio": 500
 }
 
+class Prenda:
+  def __init__(self,producto):
+    self.producto = producto
+
+  def nueva(self):
+    self.producto["precio"] = ["precio"]
+
+  def promocion(self, valor_fijo):
+    self.producto["precio"] -= valor_fijo
+
+  def liquidacion(self):
+    self.producto["precio"]/2
+
 #1.
-#registrar_producto: recibe un diccionario con codigo, nombre, categoria, precio y agrega un producto nuevo a la lista de productos. El stock del producto agregado debe estar inicialmente en cero.
+#registrar_producto: recibe un diccionario con codigo, nombre, categoria, precio y agrega un producto nuevo a la lista de productos.
 
 productos = []
 ventas = []
@@ -87,7 +97,7 @@ def cantidad_productos():
 
 
 #2.
-# recargar_stock: toma un código de producto y una cantidad de unidad de stock a agregar, e incrementa el stock correspondiente a ese producto. Si el código de producto indicado no existe, debe lanzar una excepción."""
+# recargar_stock: toma un código de producto y una cantidad de unidad de stock a agregar, e incrementa el stock correspondiente a ese producto. Si el código de producto indicado no existe, lanzauna excepción."""
 
 def recargar_stock(codigo, valor_a_agregar):
   existe = False
@@ -104,19 +114,27 @@ def recargar_stock(codigo, valor_a_agregar):
 #3.hay_stock: recibe un código de producto y dice si hay stock (es decir, si el stock correspondiente es mayor a cero). Si el código indicado no existe en la lista de productos, debe devolver False.
 
 
-def hay_stock(codigo, productos):
-    respuesta = False
-    for x in range(len(productos)):
-        if productos[x]["codigo"] == codigo and productos[x]["stock"] > 0:
-            respuesta = True
-    return respuesta
+
+def hay_stock(codigo_producto):
+  for producto in productos:
+    if codigo_producto == producto["codigo"]:
+      return producto["stock"] > 0
+  return False
+
+    #respuesta = False
+    #for x in range(len(productos)):
+        #if productos["codigo"] == codigo_producto["stock"] > 0:
+            #respuesta = True
+    #return respuesta
 
 
 #4.calcular_precio_final: toma un producto (un diccionario) y un booleano es_extranjero y calcula su valor final, según la siguiente regla: a. si quien calcula el precio es extranjero y el valor es mayor de $70, es el mismo valor sin cambios. b. en caso contrario, es el valor original más un 21%
 
 def calcular_precio_final(producto, es_extranjero):
+  precio_final=0
   if es_extranjero and producto["precio"]>70:
-    precio_final = producto ["precio"]
+    precio_final = producto["precio"]
+    return precio_final
   else:
     precio_final = producto["precio"]* 1.21
   return precio_final
@@ -143,14 +161,28 @@ def contar_categorias(productos):
 #6.realizar_compra: recibe un código de producto y una cantidad de items a comprar. En base a ello, decrementa el stock del producto correspondiente y crea una nueva venta con la información correspondiente. Si no hay suficiente stock, lanzar una excepción.
 
 
-def realizar_compra(codigo_producto, cantidad_a_comprar, fecha, ventas, productos):
-    for x in range(len(productos)):
-        if productos[x]["codigo"] == codigo_producto and productos[x]["stock"] - cantidad_a_comprar >= 0:
-            productos[x]["stock"] -= cantidad_a_comprar
-            nueva_venta = {"codigo_producto":codigo_producto, "cantidad":cantidad_a_comprar, "fecha":fecha, "precio":(cantidad_a_comprar*productos[x]["precio"])}
-            ventas.append(nueva_venta)
-        elif productos[x]["codigo"] == codigo_producto:
-            raise ValueError("Producto sin stock")
+#def realizar_compra(codigo_producto, cantidad_a_comprar, fecha, ventas, productos):
+    #for x in range(len(productos)):
+        #if productos[x]["codigo"] == codigo_producto and productos[x]["stock"] - cantidad_a_comprar >= 0:
+            #productos[x]["stock"] -= cantidad_a_comprar
+            #nueva_venta = {"codigo_producto":codigo_producto, "cantidad":cantidad_a_comprar, "fecha":fecha, "precio":(cantidad_a_comprar*productos[x]["precio"])}
+            #ventas.append(nueva_venta)
+        #elif productos[x]["codigo"] == codigo_producto:
+            #raise ValueError("Producto sin stock")
+
+def realizar_compra(codigo_producto, cantidad_a_comprar):
+  codigo_valido = False
+  for producto in productos:
+    if codigo_producto == producto["codigo"]:
+      codigo_valido=True
+      if hay_stock(codigo_producto) and producto["stock"]> cantidad_a_comprar:
+        producto["stock"] -= cantidad_a_comprar
+        monto_total= producto["precio"]*cantidad_a_comprar
+        list.append(ventas,{"producto":producto["nombre"],"cantidad_vendida":cantidad_a_comprar, "monto":monto_total,"fecha": time.strftime("%d/%m/%y"),"anio":time.strftime("%Y")})
+      else:
+        raise ValueError ("Sin stock para comprar")
+  if not codigo_valido:
+    raise ValueError ("El codigo no existe")
 
 
 
@@ -158,53 +190,71 @@ def realizar_compra(codigo_producto, cantidad_a_comprar, fecha, ventas, producto
 #7.discontinuar_productos: elimina los productos sin stock.
 
 
-def discontinuar_producto(productos):
+def discontinuar_producto():
     for producto in productos:
       if producto ["stock"]<=0:
-        productos.remove(producto)
-        
+        #productos.remove(producto)
+        list.remove(productos,producto)
 
 
 #8.valor_ventas_del_dia: retorna el valor total de las ventas del día de hoy.
 
 def valor_ventas_del_dia():
+  valor_total = 0
+  for venta in ventas:
+    if time.strftime("%d/%m/%y") == venta["fecha"]:
+      valor_total+= venta["monto"]
+  return valor_total
     
-    total_recaudado_dia = 0
-    for venta in ventas:
-        if venta["fecha"] == date.strftime("%d/%m/%y"):
-          total_recaudado += venta["precio"]
-    return total_recaudado_dia
+    #total_recaudado_dia = 0
+    #for venta in ventas:
+        #if venta["fecha"] == date.strftime("%d/%m/%y"):
+          #total_recaudado += venta["precio"]
+    #return total_recaudado_dia
 
 
 #9.ventas_del_anio: retorna un listado con todas las ventas para el año actual.
 
 def valor_ventas_del_anio():
-  total_recaudado_anio = 0
+  ventas_del_anio = 0
   for venta in ventas:
-    if date.strftime("%Y") == venta["anio"]:
-      total_ventas_anio += venta ["precio"]
-  return total_recaudado_anio
+    if time.strftime("%Y") == venta["anio"]:
+      ventas_del_anio += venta["monto"]
+  return ventas_del_anio
+
+#def valor_ventas_del_anio():
+  #total_recaudado_anio = 0
+  #for venta in ventas:
+    #if date.strftime("%Y") == venta["anio"]:
+      #total_ventas_anio += venta ["precio"]
+  #return total_recaudado_anio
 
 
                 
 
 #10.productos_mas_vendidos: toma una cantidad n de productos y retorna los nombres de los n productos más vendidos
 
-"""def productos_más_vendidos(ventas, n, productos):
-    ranking_ventas = armar_ranking_ventas(ventas)
-    ordenar_ranking_ventas(ranking_ventas)
-    x = 0
-    while x < n and x < len(ranking_ventas):
-        if existeEsteProducto(ranking_ventas[x]["código_producto"], productos):
-            producto = buscarProducto(ranking_ventas[x]["código_producto"], productos)
-            print("Nombre:",producto["nombre"])
-            print("Cantidad vendida:",ranking_ventas[x]["cantidad"])
-        x += 1"""
-
-#Ejercicio aun en correcion
+#def productos_más_vendidos(ventas, n, productos):
+    #ranking_ventas = armar_ranking_ventas(ventas)
+    #ordenar_ranking_ventas(ranking_ventas)
+    #x = 0
+    #while x < n and x < len(ranking_ventas):
+        #if existeEsteProducto(ranking_ventas[x]["código_producto"], productos):
+           # producto = buscarProducto(ranking_ventas[x]["código_producto"], productos)
+            #print("Nombre:",producto["nombre"])
+            #print("Cantidad vendida:",ranking_ventas[x]["cantidad"])
+        #x += 1"""
 
 
-#11.actualizar_precios_por_categoria: toma una categoría y un porcentaje, y actualiza según ese porcentaje el precio de todos los productos que tengan esa categoría. La búsqueda de categoría en este procedimiento no debe ser exacta: por ejemplo tanto si se pasa como argumento "REMERA", " REMERA" o "Remera", deben actualizarse los productos de la categoría "remera".
+def productos_mas_vendidos(cantidad_productos):
+  productos_vendidos= []
+  #ranking_mas_vendidos=[]
+  for venta in ventas:
+    list.append(productos_vendidos,venta["producto"])
+
+
+
+#11.actualizar_precios_por_categoria: toma una categoría y un porcentaje, y actualiza según ese porcentaje el precio de todos los productos que tengan esa categoría.
 
 def actualizar_precios_por_categoria(categoria, porcentaje, productos):
     for x in range(len(productos)):
--- test_2disenios.py
+++ test_2disenios.py
@@ -15,20 +15,20 @@ def test_recarga_10_blusas_a_la_lista():
     "nombre":"blusa",
     "categoria":"remera","precio":2900})
     recargar_stock(300, 10)
-    assert hay_stock(300, productos)
+    assert hay_stock(300)
 
 def test_si_los_2_prod_registrados_tienen_stock():
     limpiar_lista()
     registrar_producto(blusa)
     registrar_producto(short)
     recargar_stock(400, 25)
-    assert not hay_stock(300, productos)
+    assert not hay_stock(300)
 
-def test_calcular_precio_final_impuestos():
-    assert calcular_precio_final(blusa, True)==(2900)
+#def test_calcular_precio_final_impuestos():
+    #assert calcular_precio_final(blusa, True)==(2900)
 
-def test_calcular_precio_final_sin_impuestos():
-    assert calcular_precio_final(short, False) ==(2800)
+#def test_calcular_precio_final_sin_impuestos():
+    #assert calcular_precio_final(short, False) ==(2800)
 
 def test_se_agrego_compra_a_lista_ventas():
     limpiar_lista()
@@ -36,8 +36,10 @@ def test_se_agrego_compra_a_lista_ventas():
     "nombre":"blusa",
     "categoria":"remera","precio":2900})
     recargar_stock(300,10)
-    realizar_compra(300,3,12-10-2022,ventas, productos)
-    assert {'cantidad': 3, 'codigo_producto': 300, 'fecha': -2020, 'precio': 8700} in ventas
+    realizar_compra(300,3)
+    assert [{'anio': '2022', 'cantidad_vendida': 3, 'fecha': '18/10/22', 'monto': 8700, 'producto':'blusa de seda' }] in ventas
+
+    #assert {'cantidad': 3, 'codigo_producto': 300, 'fecha': -2020, 'precio': 8700} in ventas
 
 def test_se_agrego_un_producto_a_la_lista():
     limpiar_lista()
@@ -60,14 +62,14 @@ def test_realiza_compra_de_un_producto_con_stock_en_100_disminuye_a_50():
     limpiar_lista()
     registrar_producto(blazer)
     recargar_stock(80, 100)
-    realizar_compra(80, 50, 12-12-2002, ventas, productos)
+    realizar_compra(80, 50)
     assert blazer["stock"]== 50 
 
 def test_realiza_compra_de_1_producto():
     limpiar_lista()
     registrar_producto(corbata)
     recargar_stock(500, 100)
-    realizar_compra(500, 50, 12-12-2002, ventas, productos)
+    realizar_compra(500, 50)
     assert len(ventas)== 1
 
 def test_elimina_un_producto_en_la_lista_sin_stock():
@@ -83,8 +85,8 @@ def test_las_ventas_de_hoy():
     registrar_producto(campera)
     recargar_stock(600, 98)
     recargar_stock(110, 60)
-    realizar_compra(600, 3, 12/12/2022, ventas, productos)
-    realizar_compra(110, 3, 12/12/2022, ventas, productos)
+    realizar_compra(600, 3)
+    realizar_compra(110, 3)
     assert valor_ventas_del_dia
 
 
@@ -94,8 +96,8 @@ def test_ventas_de_otro_dia():
     registrar_producto(campera)
     recargar_stock(600, 98)
     recargar_stock(110, 60)
-    realizar_compra(600, 3, 12/12/2029, ventas, productos)
-    realizar_compra(110, 3, 12/12/2007, ventas, productos)
+    realizar_compra(600, 3)
+    realizar_compra(110, 3)
     assert not valor_ventas_del_dia
 
 
