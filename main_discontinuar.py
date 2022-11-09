1ab4ba2582de702eb11c148f3075799e0cd301c2
bellidopau <bellidopaulajulieta@gmail.com>
2022-11-09 14:29:53 -0300

    cambios en persistencia

--- main_discontinuar.py
+++ main_discontinuar.py
@@ -1,19 +1,18 @@
 from tp3Objetos import *
 from datetime import datetime
-from persistencia import cargar_todos
+from persistencia import *
 
 import subprocess
 
-#subprocess.call(["zenity", "--info",  '--title=Hola', "--text=¡Hola mundo!", "--display=:0"])
+#subprocess.call(["zenity", "--info",  '--title=La oprecion se realizo con exito', "--text=Sucursal con productos stockeados", "--display=:0"])
 
 
 def main():
     print(datetime.now(), "eliminando productos sin stock")
-
-    #TODO usar el modulo de persistencia
-    #sucursal = ...traernosLaSucursalReal....
-    sucursal = SucursalFisica(3)
-    sucursal.discontinuar_producto()
+    retiro = cargar("retiro")
+    retiro.discontinuar_producto()
+    retiro.cargar_todos()
+    retiro.guardar("retiro", retiro)
     print(datetime.now(), "los productos sin stock se eliminaron")
 
 if __name__ == "__main__":
