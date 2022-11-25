0655ac9045dfde6f7f041bdb53b3dfd16d0545d2
bellidopau <bellidopaulajulieta@gmail.com>
2022-11-25 11:34:23 -0300

    creando app

--- main_discontinuar.py
+++ main_discontinuar.py
@@ -8,8 +8,10 @@ import subprocess
 ejemplo_1 = "TAREA"
 
 def main():
-    print(datetime.now(), "eliminando productos sin stock")
     guardar("ejemplo_1", ejemplo_1)
+    
+    print(datetime.now(), "eliminando productos sin stock")
+    
     retiro = cargar("ejemplo_1")
     #retiro = cargar("retiro")
     print(retiro)
