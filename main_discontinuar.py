62ea49637d709f9b5bf0200c9b695142919294d7
bellidopau <bellidopaulajulieta@gmail.com>
2022-11-28 12:24:53 -0300

    persistencia

--- main_discontinuar.py
+++ main_discontinuar.py
@@ -2,26 +2,14 @@ from tp3Objetos import *
 from datetime import datetime
 from persistencia import *
 
-import subprocess
-
-#subprocess.call(["zenity", "--info",  '--title=La operacion se realizo con exito', "--text=Sucursal con productos stockeados", "--display=:0"])
-#ejemplo_1 = "sucursales"
-
 def main():
     sucursales = cargar_todos()
     for nombre in sucursales:
+      print(datetime.now(), "Discontinuando productos de la sucursal", nombre)
       sucursal1 = sucursales[nombre]
-      sucursal1.discontinuar_productos()
+      sucursal1.discontinuar_producto()
       guardar("sucursal1", sucursal1)
     
-    #guardar("sucursal_1", sucursal_rosario)
-    
-    
-    
 if __name__ == "__main__":
+  print(datetime.now(), "Discontinuando productos")
   main()
-
-"""for x in sucursal_rosario:
-     
-     sucursal_rosario.discontinuar_producto()
-     print(datetime.now(), "eliminando productos sin stock")"""
