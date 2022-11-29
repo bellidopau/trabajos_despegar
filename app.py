11cfc3fcb36d2e9c9b3f33eb17cc398c497e7552
bellidopau <bellidopaulajulieta@gmail.com>
2022-11-29 19:21:55 -0300

    nueva persistencia

--- app.py
+++ app.py
@@ -11,27 +11,16 @@ def raiz():
 
 @app.get("/lista_productos")
 def lista_de_productos():
-    rosario = cargar("rosario")
-    x = cargar_todos(modulo='tp3Objetos')
+    lista = cargar_todos(modulo='tp3Objetos')
 
-    return render_template({{url_for('lista_productos')}}, x = x)
+    return render_template("lista_productos.html", lista = lista)
 
 
 def todos_los_productos():
     productos = []
-    for sucursal in cargar_todos().values():
+    for sucursal in cargar_todos(modulo='tp3Objetos').values():
         productos += sucursal.productos
     return productos
-
-def products():
-    productos = todos_los_productos()
-    print(productos)
-    return render_template("home.html", productos_all=productos)
-
-"""@app.get("/productos/")
-def get_all_productos():
-    productos = cargar_todos()
-    return render_template("home.html", productos_all= productos)"""
 
 
 @app.get("/detalles")
@@ -48,10 +37,4 @@ def busqueda():
     busqueda = request.args.get('busqueda')
     return render_template("resultado.html")
 
-
-def todos_los_productos():
-    productos = []
-    for sucursal in cargar_todos().values():
-        productos += sucursal.productos
-    return productos
 
