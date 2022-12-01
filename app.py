c1743f038bc1aa3f11479294a8ed8cf3fec234c0
bellidopau <bellidopaulajulieta@gmail.com>
2022-12-01 10:39:26 -0300

    fix de busqueda_por_categoria, precio esta roto

--- app.py
+++ app.py
@@ -22,21 +22,31 @@ def lista_de_productos():
     return render_template("lista_productos.html", lista = cargar_sucursales())
 
 
-
-
-
 @app.get("/lista_ventas")
 def ventas():
     return render_template("lista_ventas.html")
 
 
+
+
 @app.get("/detalles")
 def detalles():
-    return render_template("detalles.html", productos = busqueda_por_nombre(request.args.get("por_nombre", "remera blanca")), busqueda = request.args.get("por_nombre"))
+    return render_template("detalles.html", productos = busqueda_por_nombre(request.args.get("por_nombre")), busqueda = request.args.get("por_nombre"))
 
 def busqueda_por_nombre(input):
     return [producto for producto in cargar_sucursales() if PorNombre(input).corresponde_al_producto(producto)]
 
+@app.get("/categoria")
+def categoria():
+    return render_template("busq_categoria.html", productos = busqueda_por_categoria(request.args.get("por_categoria")), busqueda = request.args.get("por_categoria"))
 
+def busqueda_por_categoria(input):
+    return [producto for producto in cargar_sucursales() if PorCategoria(input).corresponde_al_producto(producto)]
 
+@app.get("/precio")
+def precio():
+    return render_template("busq_precio.html", productos = busqueda_por_precio(request.args.get("por_precio")), busqueda = request.args.get("por_precio"))
+
+def busqueda_por_precio(input):
+    return [producto for producto in cargar_sucursales() if PorPrecio(input).corresponde_al_producto(producto)]
 
